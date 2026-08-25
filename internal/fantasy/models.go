6fac1e13f3dd043b816ee9ef732020ce7a063334
adrake333 <adrake333@gmail.com>
2026-08-25 15:30:52 -0500

    wrote models structs

--- internal/fantasy/models.go
+++ internal/fantasy/models.go
@@ -10,6 +10,40 @@ import (
 
 
 
+type Platform string
+
+const(
+	PlatformSleeper	Platform = "sleeper"
+	PlatformESPN	Platform = "espn"
+)
+
+type League struct {
+	ID		string		`json:"id"`
+	Name 		string 		`json:"name"`
+	Platform	Platform	`json:"platform"`
+	Season		string		`json:"season"`
+}
+
+type PlayerScore struct {
+	PlayerID	string	`json:"player_id"`
+	Name		string	`json:"name"`
+	Position	string	`json:"position"`
+	Points		float64	`json:"points"`
+}
+
+type RosterSpot struct {
+	Slot	string		`json:"slot"`
+	Player	PlayerScore	`json:"player"`
+}
+
+type Team struct {
+	ID		string		`json:"id"`
+	Name		string		`json:"name"`
+	OwnerName	string		`json:"owner_name"`
+	TotalScore	float64		`json:"total_score"`
+	Roster		[]RosterSpot	`json:"roster"`
+}
+
 type Matchup struct {
 	Leaguename	string
 	UserTeam	string
