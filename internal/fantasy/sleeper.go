ce7efc4101ee8ab1d319831f601b66e79350ed28
adrake333 <adrake333@gmail.com>
2026-08-26 22:47:18 -0500

    finished sleeper.go GetMatchups(), GetRosters(), and GetUsers()

--- internal/fantasy/sleeper.go
+++ internal/fantasy/sleeper.go
@@ -58,3 +58,67 @@ func (c *SleeperClient) GetMatchups(leagueID string, week int) ([]SleeperMatchup
 
 	return matchups, nil
 }
+
+type SleeperRoster struct {
+	RosterID	int	`json:"roster_id"`
+	OwnerID		string	`json:"owner_id"`
+}
+
+func (c *SleeperClient) GetRosters(leagueID string) ([]SleeperRoster, error) {
+	url := fmt.Sprintf("%s/league/%s/rosters", c.baseURL, leagueID)
+
+	resp, err := c.httpClient.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return nil, fmt.Errorf("sleeper API error: status %d", resp.StatusCode)
+	}
+
+	var rosters []SleeperRoster
+
+	if err := json.NewDecoder(resp.Body).Decode(&rosters); err != nil {
+		return nil, fmt.Errorf("failed to decode sleeper rosters: %w", err)
+	}
+
+	return rosters, nil
+}
+
+type SleeperUser struct {
+	UserID		string	`json:"user_id"`
+	DisplayName	string	`json:"display_name"`
+	Metadata	struct	{
+		TeamName	string	`json:"team_name"`
+	}	`json:"metadata"`
+}
+
+func (u SleeperUser) GetTeamName() string {
+	if u.Metadata.TeamName != "" {
+		return u.Metadata.TeamName
+	}
+	return u.DisplayName
+}
+
+func (c *SleeperClient) GetUsers(leagueID string) ([]SleeperUser, error) {
+	url := fmt.Sprintf("%s/league/%s/users", c.baseURL, leagueID)
+
+	resp, err := c.httpClient.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return nil, fmt.Errorf("sleeper API error: status %d", resp.StatusCode)
+	}
+
+	var users []SleeperUser
+
+	if err := json.NewDecoder(resp.Body).Decode(&users); err != nil {
+		return nil, fmt.Errorf("failed to decode sleeper users: %w", err)
+	}
+
+	return users, nil
+}
